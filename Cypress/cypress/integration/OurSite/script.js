e6edf0ce161d7c9b8c54cff3091fa387b49da50f
diabsadi98 <dibo.sa3di@hotmail.com>
2021-05-27 17:18:42 -0400

    Upload

---
+++ Cypress/cypress/integration/OurSite/script.js
@@ -0,0 +1,22 @@
+const { it } = require("mocha");
+
+describe("Blink" , ()=>{
+    it("Shopping and add to cart with sign in",()=>{
+        cy.visit("blink.byethost12.com/");
+    cy.get('[href=user/signIn.html]').click();
+    cy.get('[type=email').type("diab.sadi98@gmail.com");
+    cy.get('[type=password').type("diab123");
+    cy.get('[type=submit]').click();
+    cy.get('.b8').click();
+    cy.get('.item:nth(1)').click();
+    cy.get('[href=cart.php]').clikc();
+    cy,get('#btntotal').click();
+    
+    })
+    it("Log out" , ()=>{
+        cy.visit("blink.byethost12.com/user/index.php");
+        cy.get(['herf=logout.php']).click() 
+       })
+    
+}
+)

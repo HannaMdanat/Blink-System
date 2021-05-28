951a94bae3a27bd3985dace49c8746f847d5b41a
diabsadi98 <dibo.sa3di@hotmail.com>
2021-05-27 20:15:09 -0400

    Upload

--- user/addUser.php
+++ user/addUser.php
@@ -1,15 +1,22 @@
 <?php
      $name = $_POST["username"];
-     $pass = $_GET["password"];
+     $pass = $_POST["password"];
      $amount = 500;
-     include '../../connectiondb.php';
-    $sql = "INSERT INTO users VALUES ('$name','$pass','$amount')";
+     include 'connectiondb.php';
+     if($link){
+        echo '<script type="text/javascript"> window.alert("Done !!");</script>';
+     }
+     else{
+        echo '<script type="text/javascript"> window.alert("Down !!");</script>';
+
+     }
+    $sql = "INSERT INTO users (user,password,amout) VALUES ('$name','$pass','$amount')";
     if(mysqli_query($link, $sql)){
         echo '<script type="text/javascript"> window.alert("Welcome !!");</script>';
         echo '<script type="text/javascript"> window.open("index.php","_self"); </script>';
     }
     else{
-        echo '<script type="text/javascript"> window.alert("Error!!");</script>';
+        echo '<script type="text/javascript"> window.alert("mysqli_error($link)");</script>';
         echo '<script type="text/javascript"> window.open("register.php","_self"); </script>'; 
     }
 ?>
--- user/register.php
+++ user/register.php
@@ -3,9 +3,9 @@
   <title>Register</title>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/nav.css">
-  <link rel="stylesheet" href="css/logIn.css">
-  <link rel="shortcut icon"  type="image/x-icon"  href="img/image14.png">
+  <link rel="stylesheet" href="../css/nav.css">
+  <link rel="stylesheet" href="../css/logIn.css">
+  <link rel="shortcut icon"  type="../image/x-icon"  href="../img/image14.png">
 </head>
 
 <body>
@@ -19,7 +19,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0 navbraroptions">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Contact us</a>
@@ -39,7 +39,7 @@
 
   <div class="b1">
     <div class="i1">
-      <img src="img/image12.png" height="150px" width="155px">
+      <img src="../img/image12.png" height="150px" width="155px">
       <h2 style="margin-bottom:80px;">Welcome</h2>
       <form action="addUser.php" method="POST">
       <input type="email"  class="form-control" required placeholder="E-Mail" name="username">
